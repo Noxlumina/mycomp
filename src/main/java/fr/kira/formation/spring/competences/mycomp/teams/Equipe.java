22d1be2b4906197d02fb5ee64728ad0a58946003
Xavier Agache <xav.agache@gmail.com>
2022-12-01 12:11:18 +0100

    <chores> : ajout des personnes dans les équipes

--- src/main/java/fr/kira/formation/spring/competences/mycomp/teams/Equipe.java
+++ src/main/java/fr/kira/formation/spring/competences/mycomp/teams/Equipe.java
@@ -1,9 +1,14 @@
 package fr.kira.formation.spring.competences.mycomp.teams;
 
+import fr.kira.formation.spring.competences.mycomp.persons.Personne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document
 @NoArgsConstructor
@@ -12,4 +17,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Equipe {
     private String id;
     private String nom;
+    @DBRef
+    private List<Personne> membres = new ArrayList<Personne>();
 }
